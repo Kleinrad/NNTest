49979113c541e8ab1dd281b0e07eb3371219d110
Kleinrad <fabiankleinrad.fk@gmail.com>
2020-01-19 17:06:25 +0100

    19 1 2020
    Load Image
    Train Icon

--- NNtest/src/TestEnviroment/NNTestEnviroment.java
+++ NNtest/src/TestEnviroment/NNTestEnviroment.java
@@ -5,19 +5,13 @@
  */
 package TestEnviroment;
 
-import java.awt.Color;
 import java.awt.Component;
-import java.awt.Container;
 import java.awt.Cursor;
-import java.io.File;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.BorderFactory;
-import javax.swing.JFileChooser;
-import javax.swing.JMenu;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
-import javax.swing.filechooser.FileNameExtensionFilter;
 
 
 /**
@@ -45,6 +39,8 @@ public class NNTestEnviroment extends javax.swing.JFrame {
         mainPageCenter1 = new TestEnviroment.MainPageCenter();
         cMenuBar1 = new TestEnviroment.CMenuBar();
         fileMenu = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
         editMenu = new javax.swing.JMenu();
         viewMenu = new javax.swing.JMenu();
         previewMenu = new javax.swing.JMenu();
@@ -61,7 +57,24 @@ public class NNTestEnviroment extends javax.swing.JFrame {
         });
         getContentPane().add(mainPageCenter1, java.awt.BorderLayout.CENTER);
 
+        fileMenu.setMnemonic('F');
         fileMenu.setText("File");
+
+        jMenuItem1.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_S, java.awt.event.InputEvent.CTRL_MASK));
+        jMenuItem1.setMnemonic('S');
+        jMenuItem1.setText("Save Instance");
+        jMenuItem1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem1ActionPerformed(evt);
+            }
+        });
+        fileMenu.add(jMenuItem1);
+
+        jMenuItem2.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_O, java.awt.event.InputEvent.CTRL_MASK));
+        jMenuItem2.setMnemonic('L');
+        jMenuItem2.setText("Load Instance");
+        fileMenu.add(jMenuItem2);
+
         cMenuBar1.add(fileMenu);
 
         editMenu.setText("Edit");
@@ -113,6 +126,10 @@ public class NNTestEnviroment extends javax.swing.JFrame {
         mainPageCenter1.increaseScale();
         mainPageCenter1.refresh();
     }//GEN-LAST:event_increaseScaleActionPerformed
+
+    private void jMenuItem1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jMenuItem1ActionPerformed
 
     private void init (){
         cMenuBar1.setColor(EnvUtils.barColor);
@@ -173,6 +190,8 @@ public class NNTestEnviroment extends javax.swing.JFrame {
     private javax.swing.JMenu editMenu;
     private javax.swing.JMenu fileMenu;
     private javax.swing.JMenuItem increaseScale;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
     private TestEnviroment.MainPageCenter mainPageCenter1;
     private javax.swing.JMenu previewMenu;
     private javax.swing.JMenu viewMenu;
